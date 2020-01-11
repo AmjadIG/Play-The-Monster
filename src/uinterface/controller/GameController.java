61d53eeb56e5a32b62e886e72dfe1ca8e9b5d852
AmjadIG <amjadmenouer@gmail.com>
2020-01-11 20:42:50 +0100

    GameController

--- src/uinterface/controller/GameController.java
+++ src/uinterface/controller/GameController.java
@@ -2,13 +2,29 @@ package uinterface.controller;
 
 import businesslogic.client.Facade;
 
+import javax.crypto.NoSuchPaddingException;
 import java.lang.reflect.InvocationTargetException;
+import java.security.NoSuchAlgorithmException;
 
 public class GameController {
 
     Facade facade;
 
     public GameController(){}
+
+    public void signIn(String name, String password) throws NoSuchPaddingException, NoSuchAlgorithmException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        facade.interpreteAction("#signIn/"+name+","+hash(password));
+    }
+
+    public void signUp(String name, String password) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        facade.interpreteAction("#signUp/"+name+","+hash(password));
+    }
+
+    //TODO (Should use bcrypt or whatever but don't had the time
+    public String hash(String password){
+        return "Bfrjvn4543Fdf"+password+"jvn4B656vfdER";
+    }
+
 
     public void createGame() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         facade.interpreteAction("#createGame/");
