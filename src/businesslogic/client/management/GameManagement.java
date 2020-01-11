fe2516a99a3a01876134c4b9f2c46440a3990c4f
AmjadIG <amjadmenouer@gmail.com>
2020-01-11 22:02:55 +0100

    Management package

--- src/businesslogic/client/management/GameManagement.java
+++ src/businesslogic/client/management/GameManagement.java
@@ -1,9 +1,20 @@
 package businesslogic.client.management;
 
-import persistlayer.DAO.*;
+import persistlayer.DAO.DAO;
+import persistlayer.DAO.DAOFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameManagement {
     private DAO userDAO = DAOFactory.getUserDAO();
 
+    //TODO
+    public void signIn(String name, String password) {
+        Map map = new HashMap(); map.put("name",name); map.put("password",password);
 
+        if (userDAO.getBy(map).equals(null)){
+
+        }
+    }
 }
