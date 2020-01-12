6d77ba698004f9d697942e917fe92a1b16a0e1c7
AmjadIG <amjadmenouer@gmail.com>
2020-01-12 16:54:20 +0100

    Game controller + Manage interface issues

--- src/uinterface/controller/GameController.java
+++ src/uinterface/controller/GameController.java
@@ -2,84 +2,78 @@ package uinterface.controller;
 
 import businesslogic.client.Facade;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.security.NoSuchAlgorithmException;
 
 public class GameController extends Application {
     Facade facade;
-    Stage stage = null;
+    Parent parent = null;
     Scene scene = null;
-    Button submitUp = null;
-    Button submitIn = null;
+
+    //Sign In Attributes
+    @FXML
     TextField name = null;
+    @FXML
     TextField password = null;
 
-    public GameController(){
-
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(GameController.class.getResource("/uinterface/views/login.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        scene = new Scene(root, 600, 330);
-        name = (TextField) scene.lookup("#loginTextField");
-        password = (TextField) scene.lookup("#pwdTextField");
-        submitIn = (Button) scene.lookup("#loginBtn");
-        /*
-        submitIn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                try {
-                    signIn(name.getText(), password.getText());
-                } catch (NoSuchPaddingException e) {
-                    e.printStackTrace();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
-        });*/
-    }
+    //Register Attributes
+    @FXML
+    TextField playerName = null;
+    @FXML
+    TextField password1 = null;
+    @FXML
+    TextField password2 = null;
+    @FXML
+    TextField email = null;
 
     @Override
     public void start(Stage arg0) throws Exception {
+        try {
+            parent = FXMLLoader.load(GameController.class.getResource("../views/login.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scene = new Scene(parent, 600, 330);
+        name = (TextField) scene.lookup("#loginTextField");
+        password = (TextField) scene.lookup("#pwdTextField");
         arg0.setTitle("Login");
         arg0.setScene(this.scene);
         arg0.show();
     }
 
-    public void signIn(String name, String password) throws NoSuchPaddingException, NoSuchAlgorithmException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        facade.interpreteAction("#signIn/"+name+","+hash(password));
+    public static void main(String args[]){
+        Application.launch(args);
     }
 
-    public void signUp(String name, String password) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        facade.interpreteAction("#signUp/"+name+","+hash(password));
+    public void signIn(MouseEvent mouseEvent) throws ClassNotFoundException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException {
+        facade.interpreteAction("#signIn/"+name+","+hash(password.getText()));
     }
 
-    //TODO (Should use bcrypt or whatever but don't had the time
+    public void signUp(MouseEvent mouseEvent) throws IOException {
+        changeScene("../views/loginRegister.fxml");
+    }
+
+    public void register(MouseEvent mouseEvent) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException {
+        String pn = playerName.getText();
+        String p1 = hash(password1.getText());
+        String p2 = hash(password2.getText());
+        String mail = email.getText();
+
+        facade.interpreteAction("#signup/"+pn+","+p1+","+p2+","+mail);
+    }
+
+    //(We Should use BCrypt or any encryption script to encrypt our password (but lack of time)
     public String hash(String password){
-        return "Bfrjvn4543Fdf"+password+"jvn4B656vfdER";
+        return "Bfrjvn4543Fdf"+password+"jvn4B656vfdER"; //So we added salt (at the end and at the beginning)
     }
-
 
     public void createGame() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         facade.interpreteAction("#@createGame/");
@@ -97,12 +91,17 @@ public class GameController extends Application {
         facade.interpreteAction("#saveGame/");
     }
 
-    //TODO
-    public void getGameResponse(){}
+    public void changeScene(String fxml) throws IOException {
+        Stage newStage = new Stage();
+        parent = FXMLLoader.load(getClass().getResource(fxml));
+        scene = new Scene(parent, 600, 330);
+        newStage.setScene(scene);
 
-    public static void main(String args[]){
-        GameController displayControl = new GameController();
-        Application.launch(args);
+        playerName = (TextField) scene.lookup("#playerName");
+        password1 = (TextField) scene.lookup("#password1");
+        password2 = (TextField) scene.lookup("#password2");
+        email = (TextField) scene.lookup("#email");
+
+        newStage.show();
     }
-
 }
